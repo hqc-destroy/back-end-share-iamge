bc3ee3ed4889539acace77425dca850feb8c28f1
Nguyen Ngoc Hai <ngochai220998@gmail.com>
2020-06-04 23:38:21 +0700

    update

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,9 +1,9 @@
-const chatbox = require('./chatbox/index')
+const user = require('./user/index')
 
 module.exports = {
     run: function run(socket, io) {
         socket.join(socket.request._query.name)
-        console.log(socket.request._query.name)
-        chatbox(socket, io)
+        // console.log(socket.request._query.name)
+        user(socket, io)
     }
 }
