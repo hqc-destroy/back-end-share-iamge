f0c308f764ba516d56838ef34da33354e08f7533
theanh17071998 <nguyentheanh17071998@gmail.com>
2020-06-11 22:01:51 +0700

    fix conflict routes/index.js constans/api.js

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,8 +1,10 @@
 module.exports = {
     ROOT_API: {
         USER: '/api/users', // /api/user -> /api/users
-<<<<<<< HEAD
-        COMMENT: '/api/comments' // /api/comment -> /api/comments
+
+        COMMENT: '/api/comments', // /api/comment -> /api/comments
+
+        IMAGE: '/api/images'
     },
     USER: {
         CREATE_USER: '/', // /create-user -> /  note: Create a new user (method: post)
@@ -10,21 +12,14 @@ module.exports = {
     },
     COMMENT: {
         CREATE_COMMENT: '/', // /create-comment -> / note: Create a new commnet
-        UPDATE_COMMENT: '/:id',// update-comment -> / note: update a comment
+        UPDATE_COMMENT: '/:id/:userId',// update-comment -> / note: update a comment
         DELETE_COMMENT: '/:id/:userId', // delete-comment -> / note: delete a commnent
-        GET_COMMENT: '/'
-=======
-        IMAGE: '/api/images'
-    },
-    USER: {
-        CREATE_USER: '/', // /create-user -> /  note: Create a new user (method: post)
-        LOGIN: '/login',
+        GET_COMMENT: '/'   
     },
     IMAGE: {
         GET_IMAGES: '/', 
         CREATE_IMAGE: '/', // /create-image -> / note: Create a new image theo userId (method: post)
         UPDATE_IMAGE: '/:id/:userId', // /update-image -> / note: Update a new image theo userId (method: put)
         DELETE_IMAGE: '/:id/:userId'
->>>>>>> feature/image
     }
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,11 @@
 
 const constants = require('../constants/api')
 const userRoute = require('./user/index')()
-<<<<<<< HEAD
 const commentRoute = require('./comment/index')()
-
-module.exports = (app) => {
-  app.use(constants.ROOT_API.USER, userRoute)
-  app.use(constants.ROOT_API.COMMENT, commentRoute)
-=======
 const imageRoute = require('./image/index')()
 
 module.exports = (app) => {
   app.use(constants.ROOT_API.USER, userRoute)
+  app.use(constants.ROOT_API.COMMENT, commentRoute)
   app.use(constants.ROOT_API.IMAGE, imageRoute)
->>>>>>> feature/image
 }
